03a0ff4a388e8245663a1d4965b5c4c66a9dfcc3
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2025-07-22 13:47:18 +0200

    gbaCentrality.* : create module, mostly just SPEC at this point

---
+++ gbaCentrality.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "gbaCentrality.h"
+#include "mem.h"
+
+
+geneScores *gbaCentrality(adjacencyMatrix *A, geneScores *causal, float alpha) {
+    // sanity check:
+    if (A->nbCols != causal->nbGenes) {
+	fprintf(stderr, "");
+	exit(1);
+    }
+
+    geneScores *scores = mallocOrDie(sizeof(geneScores), "E: OOM for scores");
+    scores->nbGenes = causal->nbGenes;
+    scores->scores = mallocOrDie(causal->nbGenes * sizeof(float), "E: OOM for scores->scores");
+
+    // TODO: code GBA-centrality algorithm
+    
+    return(scores);
+}
+
+
+void printScores(geneScores *scores) {
+    float *currentScoreP = scores->scores;
+    for (unsigned int i = 0; i < scores->nbGenes; i++) {
+	printf("%f\n", *currentScoreP);
+	currentScoreP++;
+    }
+}
+
+
+void freeScores(geneScores *scores) {
+    free(scores->scores);
+    free(scores);
+}
+
---
+++ gbaCentrality.h
@@ -0,0 +1,31 @@
+#ifndef _GBACENTRALITY_H_
+#define _GBACENTRALITY_H_
+
+#include "adjacency.h"
+
+
+/*
+    Store one score per gene, each score is a float >=0.
+    For defining causal genes, each score must be in [0,1] and most should be 0.
+    scores MUST be large enough to store nbGenes floats.
+ */
+typedef struct {
+    unsigned int nbGenes;
+    float *scores;
+} geneScores;
+
+
+/*
+    Given a network represented by A and some seed nodes (eg known causal genes),
+    apply the GBA-centrality algorithm to calculate a score for each node in
+    the network.
+    "alpha" is the GBA-centrality parameter, it must be in ]0,1[, typically 0.5 is good.
+*/
+geneScores *gbaCentrality(adjacencyMatrix *A, geneScores *causal, float alpha);
+
+
+void printScores(geneScores *scores);
+
+void freeScores(geneScores *scores);
+
+#endif
