fcc03f7da644300a5df78a01643879b0ad1f9ea8
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2025-10-01 15:51:18 +0200

    scores: use SCORETYPE rather than hard-coded float

--- scores.h
+++ scores.h
@@ -2,13 +2,16 @@
 #define _SCORES_H_
 
 
+// type used for each geneScores->scores value
+#define SCORETYPE float
+
 /*
     Store one score per gene, each score is a float >=0.
     scores MUST be large enough to store nbGenes floats.
  */
 typedef struct {
     unsigned int nbGenes;
-    float *scores;
+    SCORETYPE *scores;
 } geneScores;
 
 
