f30cecd4c7f07fe46ef1fcdb1e974cb90f51ff8e
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2025-10-02 13:55:01 +0200

    compactAdjacency: switch from adjacency to network input datatype

--- compactAdjacency.c
+++ compactAdjacency.c
@@ -1,67 +1,67 @@
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
+#include <assert.h>
 
 #include "compactAdjacency.h"
-#include "adjacency.h"
+#include "network.h"
 #include "mem.h"
 
 
-compactAdjacencyMatrix *adjacency2compact(adjacencyMatrix *A) {
-    int checkA = checkAdjacency(A);
-    if (checkA == 1) {
-        fprintf(stderr, "E: weights are not in [0, 1], please fix the network\n");
+compactAdjacencyMatrix *network2compact(network *N) {
+    int nbSelfLoops = checkNetwork(N);
+    if (nbSelfLoops == -1) {
+        fprintf(stderr, "E: weights are not in ]0, 1], please fix the network\n");
         exit(1);
-    } else if (checkA == 2) {
-        fprintf(stderr, "W: your network has self-loops, they have been removed\n");
+    } else if (nbSelfLoops > 0) {
+        fprintf(stderr, "W: your network has %i self-loops, they have been removed\n", nbSelfLoops);
     }
 
     compactAdjacencyMatrix *compact = mallocOrDie(sizeof(compactAdjacencyMatrix), "E: OOM for compact\n");
 
-    compact->nbNodes = A->nbCols;
-    compact->offsets = mallocOrDie(sizeof(size_t) * (A->nbCols + 1), "E: OOM for offsets\n");
-    
-    compact->offsets[0] = 0;
+    compact->nbNodes = N->nbNodes;
+    compact->offsets = mallocOrDie(sizeof(size_t) * (N->nbNodes + 1), "E: OOM for offsets\n");
+    compact->predecessors = mallocOrDie(sizeof(unsigned int) * (N->nbEdges - nbSelfLoops),
+                                        "E: OOM for predecessors\n");
+    compact->weights = mallocOrDie(sizeof(float) * (N->nbEdges - nbSelfLoops), "E: OOM for weights\n");
+    compact->offsetsReverseEdge = mallocOrDie(sizeof(size_t) * (N->nbEdges - nbSelfLoops),
+                                              "E: OOM for offsetsReverseEdge\n");
+
     size_t sumInDegrees = 0;
-    for (size_t j = 0; j < A->nbCols; j++) {
-        // in-degree of node j
-        size_t inDegree = 0;
-        for (size_t i = 0; i < A->nbCols; i++) {
-            if (A->weights[i * A->nbCols + j] > 0) {
-                inDegree++;
-            }
+    unsigned int currentDest = 0;
+    edge *currentEdgeP = N->edges;
+    compact->offsets[0] = 0;
+
+    for (size_t ei = 0; ei < N->nbEdges; ei++) {
+        // ignore zero-weight self-loops
+        if (currentEdgeP->weight == 0) {
+            currentEdgeP++;
+            continue;
         }
-        sumInDegrees += inDegree;
-        compact->offsets[j+1] = sumInDegrees;
+        while (currentDest < currentEdgeP->dest)
+            compact->offsets[++currentDest] = sumInDegrees;
+        compact->predecessors[sumInDegrees] = currentEdgeP->source;
+        compact->weights[sumInDegrees] = currentEdgeP->weight;
+
+        sumInDegrees++;
+        currentEdgeP++;
     }
+    while (currentDest < compact->nbNodes)
+        // set offsets[nbNodes] & also if some of the last nodes have no incoming edges
+        compact->offsets[++currentDest] = sumInDegrees;
 
-    compact->predecessors = mallocOrDie(sizeof(unsigned int) * sumInDegrees, "E: OOM for predecessors\n");
-    compact->weights = mallocOrDie(sizeof(float) * sumInDegrees, "E: OOM for weights in compact\n");
+    // sanity check
+    assert(sumInDegrees + nbSelfLoops == N->nbEdges);
 
-    size_t idxPred = 0;
-    for (size_t j = 0; j < A->nbCols; j++) {
-        for (size_t i = 0; i < A->nbCols; i++) {
-            if (A->weights[i * A->nbCols + j] > 0) {
-                compact->predecessors[idxPred] = i;
-                compact->weights[idxPred] = A->weights[i * A->nbCols + j];
-                idxPred++;
-            }
-        }
-    }
-
-    compact->offsetsReverseEdge = mallocOrDie(sizeof(size_t) * sumInDegrees,
-                                              "E: OOM for offsetsReverseEdge in compact\n");
-    for (unsigned int j = 0; j < A->nbCols; j++) {
+    // fill offsetsReverseEdge
+    for (unsigned int j = 0; j < N->nbNodes; j++) {
         for (size_t offset = compact->offsets[j]; offset < compact->offsets[j + 1]; offset++) {
             unsigned int p = compact->predecessors[offset];
-            // find offset of j->p edge if it exists
+            // find offset of j->p edge if it exists, ie look for j among the predecessors of p
             size_t offsetReverseEdge = sumInDegrees;
-            if (A->weights[j * A->nbCols + p] > 0) {
-                // look for j among the predecessors of p
-                for (size_t offsetRev = compact->offsets[p]; offsetRev < compact->offsets[p + 1]; offsetRev++) {
-                    if (compact->predecessors[offsetRev] == j) {
-                        offsetReverseEdge = offsetRev;
-                        break;
-                    }
+            for (size_t offsetRev = compact->offsets[p]; offsetRev < compact->offsets[p + 1]; offsetRev++) {
+                if (compact->predecessors[offsetRev] == j) {
+                    offsetReverseEdge = offsetRev;
+                    break;
                 }
             }
             compact->offsetsReverseEdge[offset] = offsetReverseEdge;
