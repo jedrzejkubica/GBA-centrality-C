76589b15a51624529bef092628cf104de94f7153
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2025-10-02 13:37:32 +0200

    checkNetwork(): sort edges, return number of removed self-loops

--- network.c
+++ network.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 #include "network.h"
 #include "mem.h"
 
-int checkNetwork(network *N) {
+
+/*
+  compare two edges so they can be sorted by increasing dest then
+  increasing source
+ */
+int compEdges(const void *elem1, const void *elem2) {
     int retVal = 0;
+    edge *edge1 = (edge*) elem1;
+    edge *edge2 = (edge*) elem2;
+
+    if (edge1->dest < edge2->dest)
+        retVal = -1;
+    else if (edge1->dest > edge2->dest)
+        retVal = 1;
+    else if (edge1->source < edge2->source)
+        retVal = -1;
+    else if (edge1->source > edge2->source)
+        retVal = 1;
+    return(retVal);
+}
+
+int checkNetwork(network *N) {
+    int nbSelfLoops = 0;
 
     edge *edgeP = N->edges;
     for (size_t i = 0; i < N->nbEdges; i++) {
-        // remove self-loops
-        if (edgeP->source == edgeP->dest) {
-            edgeP->weight = 0;
-            retVal = 2;
-        }
         // check if weights are in ]0, 1]
         if ((edgeP->weight <= 0) || (edgeP->weight > 1)) {
             // immediately return, we can't do anything with this network
-            return(1);
+            return(-1);
+        }
+        // remove self-loops
+        if (edgeP->source == edgeP->dest) {
+            edgeP->weight = 0;
+            nbSelfLoops++;
         }
         edgeP++;
     }
-    return(retVal);
+    // sort the edges in-place
+    qsort(N->edges, N->nbEdges, sizeof(edge), compEdges);
+    return(nbSelfLoops);
 }
 
 void freeNetwork(network *N) {
