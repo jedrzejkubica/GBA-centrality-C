026dd46b1e30c03951ac0329f6dc7417057bed8d
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2026-02-09 21:37:43 +0100

    testAdjacency.c: improve doc and TEST RESULTS against expected

--- testAdjacency.c
+++ testAdjacency.c
@@ -17,65 +17,79 @@
 */
 
 #include <stdio.h>
+#include <math.h>
 
 #include "network.h"
 #include "scores.h"
 #include "gbaCentrality.h"
 #include "mem.h"
 
+// for comparing obtained scores with constant floats
+#define EPSILON 1e-6
 
+/* weighted direct network: B <- A -> C, weights 0.5 and 1, A is the seed */
+network *threeWeighted(void) {
+    network *threeWeighted = mallocOrDie(sizeof(network), "E: OOM for threeWeighted");
+    threeWeighted->nbNodes = 3;
+    threeWeighted->nbEdges = 2;
+    threeWeighted->edges = mallocOrDie(threeWeighted->nbEdges * sizeof(edge), "E: OOM for threeWeighted edges");
+    edge *edgeP = threeWeighted->edges;
+
+    edgeP->source = 0;
+    edgeP->dest = 1;
+    edgeP->weight = 0.5;
+    edgeP++;
+    edgeP->source = 0;
+    edgeP->dest = 2;
+    edgeP->weight = 1;
+    edgeP++;
+    return(threeWeighted);
+}
+geneScores *causalGenesThreeWeighted(void) {
+    geneScores *causal = mallocOrDie(sizeof(geneScores), "E: OOM for causal genes");
+    causal->nbGenes = 3;
+    causal->scores = mallocOrDie(causal->nbGenes * sizeof(SCORETYPE), "E: OOM for causal genes");
+
+    for (size_t i = 0; i < causal->nbGenes; i++)
+        causal->scores[i] = 0;
+    causal->scores[0] = 1;
+    return(causal);
+}
+
+/* weighted direct network: A-> B, A->C, B->D, C->D, weights 0.1 for A->B and 1 for all others,
+   one self-interaction D->D (should get removed), A is the seed */
 network *diamond4(void) {
-    // diamond network for testing
     network *diamond4 = mallocOrDie(sizeof(network), "E: OOM for diamond4");
     diamond4->nbNodes = 4;
-    diamond4->nbEdges = 9;
+    diamond4->nbEdges = 5;
     diamond4->edges = mallocOrDie(diamond4->nbEdges * sizeof(edge), "E: OOM for diamond4 edges");
     edge *edgeP = diamond4->edges;
 
     edgeP->source = 0;
     edgeP->dest = 1;
-    edgeP->weight = 0.2;
+    edgeP->weight = 0.1;
     edgeP++;
     edgeP->source = 0;
     edgeP->dest = 2;
-    edgeP->weight = 0.5;
+    edgeP->weight = 1;
     edgeP++;
 
-    edgeP->source = 1;
-    edgeP->dest = 0;
-    edgeP->weight = 0.4;
-    edgeP++;
     edgeP->source = 1;
     edgeP->dest = 3;
     edgeP->weight = 1;
     edgeP++;
 
     edgeP->source = 2;
-    edgeP->dest = 0;
-    edgeP->weight = 1;
-    edgeP++;
-    edgeP->source = 2;
     edgeP->dest = 3;
-    edgeP->weight = 1;
-    edgeP++;
-
-    edgeP->source = 3;
-    edgeP->dest = 1;
-    edgeP->weight = 1;
-    edgeP++;
-    edgeP->source = 3;
-    edgeP->dest = 2;
     edgeP->weight = 1;
     edgeP++;
     
-    // add a self-interaction 3->3 for testing
+    // self-interaction 3->3 for testing that it is removed
     edgeP->source = 3;
     edgeP->dest = 3;
     edgeP->weight = 1;
     return(diamond4);
 }
-
-
 geneScores *causalGenesDiam4(void) {
     geneScores *causal = mallocOrDie(sizeof(geneScores), "E: OOM for causal genes");
     causal->nbGenes = 4;
@@ -83,17 +97,15 @@ geneScores *causalGenesDiam4(void) {
 
     for (size_t i = 0; i < causal->nbGenes; i++)
         causal->scores[i] = 0;
-
-    //causal->scores[1] = 1;
-    causal->scores[2] = 1;
-
+    causal->scores[0] = 1;
     return(causal);
 }
 
 
+/* TODO document this diamond6 test: what network, what seeds?
+   All we know for now is: "node 1 without edges and node 2 with only a self-loop"
+*/
 network *diamond6(void) {
-    // diamond network for testing
-    // add node 1 without edges and node 2 with only a self-loop
     network *diamond6 = mallocOrDie(sizeof(network), "E: OOM for diamond6");
     diamond6->nbNodes = 6;
     diamond6->nbEdges = 10;
@@ -136,21 +148,18 @@ network *diamond6(void) {
     edgeP->weight = 1;
     edgeP++;
     
-    // add a self-interaction 3->3 for testing
+    // add self-interactions 2->2 and 3->3
+    edgeP->source = 2;
+    edgeP->dest = 2;
+    edgeP->weight = 1;
+    edgeP++;
     edgeP->source = 3;
     edgeP->dest = 3;
     edgeP->weight = 1;
     edgeP++;
 
-    // add a self-interaction 2->2 for testing
-    edgeP->source = 2;
-    edgeP->dest = 2;
-    edgeP->weight = 1;
-
     return(diamond6);
 }
-
-
 geneScores *causalGenesDiam6(void) {
     geneScores *causal = mallocOrDie(sizeof(geneScores), "E: OOM for causal genes");
     causal->nbGenes = 6;
@@ -158,17 +167,15 @@ geneScores *causalGenesDiam6(void) {
 
     for (size_t i = 0; i < causal->nbGenes; i++)
         causal->scores[i] = 0;
-
-    //causal->scores[1] = 1;
+    causal->scores[1] = 1;
     causal->scores[4] = 1;
-
     return(causal);
 }
 
 
 /*
   7 nodes: node 1 is connected to nodes 0 and 2, node 2 is a hub connected to 1 and 3-6,
-  and we create this as an undirected unweighted network
+  and we create this as an undirected unweighted network. Node 1 will be the only seed.
 */
 network *asymmetric(void) {
     network *asym = mallocOrDie(sizeof(network), "E: OOM for asym");
@@ -230,7 +237,6 @@ network *asymmetric(void) {
     
     return(asym);
 }
-
 geneScores *causalGenesAsym(void) {
     geneScores *causal = mallocOrDie(sizeof(geneScores), "E: OOM for causal genes");
     causal->nbGenes = 7;
@@ -238,14 +244,39 @@ geneScores *causalGenesAsym(void) {
 
     for (size_t i = 0; i < causal->nbGenes; i++)
         causal->scores[i] = 0;
-
-    //causal->scores[1] = 1;
-    causal->scores[2] = 1;
-
+    causal->scores[1] = 1;
     return(causal);
 }
 
 int main(void) {
+    printf("\n#############################################################################\n");
+    {
+        // threeWeighted
+        network *threeW = threeWeighted();
+        printf("threeWeighted network\n");
+        printNetwork(threeW);
+
+        geneScores *causal = causalGenesThreeWeighted();
+        printf("threeWeighted causal genes:\n");
+        printScores(causal);
+        
+        geneScores *result = mallocOrDie(sizeof(geneScores), "E: OOM for result scores");
+        result->nbGenes = causal->nbGenes;
+        result->scores = mallocOrDie(causal->nbGenes * sizeof(SCORETYPE), "E: OOM for result scores");
+
+        gbaCentrality(threeW, causal, 0.5, result);
+        printf("threeWeighted scores\n");
+        printScores(result);
+        // compare with expected final scores
+        if ((result->scores[0] == 1) && (result->scores[1] == 0.25) && (result->scores[2] == 0.5))
+            printf("AOK, SCORES ARE AS EXPECTED\n");
+        else
+            printf("ERROR: SCORES FOR threeWeighted network ARE WRONG!\n");
+        freeNetwork(threeW);
+        freeScores(causal);
+        freeScores(result);
+    }
+    printf("\n#############################################################################\n");
     {
         // dimaond4 network
         network *diam4 = diamond4();
@@ -263,12 +294,18 @@ int main(void) {
         gbaCentrality(diam4, causal, 0.5, result);
         printf("Diam4 scores\n");
         printScores(result);
+        // compare with expected final scores
+        if ((result->scores[0] == 1) && (fabsf(result->scores[1] - 0.05f) < EPSILON) &&
+            (result->scores[2] == 0.5) && (fabsf(result->scores[3] - 0.1375f) < EPSILON))
+            printf("AOK, SCORES ARE AS EXPECTED\n");
+        else
+            printf("ERROR: SCORES FOR diam4 network ARE WRONG!\n");
         freeNetwork(diam4);
         freeScores(causal);
         freeScores(result);
     }
-
-        {
+    printf("\n#############################################################################\n");
+    {
         // dimaond6 network
         network *diam6 = diamond6();
         printf("diam6 with its self-loop\n");
@@ -284,12 +321,14 @@ int main(void) {
 
         gbaCentrality(diam6, causal, 0.5, result);
         printf("diam6 scores\n");
+        // TODO compare with expected
         printScores(result);
+        printf("DUNNO WHAT TO EXPECT, FIXME!\n");
         freeNetwork(diam6);
         freeScores(causal);
         freeScores(result);
     }
-
+    printf("\n#############################################################################\n");
     {
         // asymmetric network
         network *asym = asymmetric();
@@ -297,7 +336,7 @@ int main(void) {
         printNetwork(asym);
     
         geneScores *causal = causalGenesAsym();
-        printf("Asym causal genes:\n");
+        printf("ASYM causal genes:\n");
         printScores(causal);
 
         geneScores *result = mallocOrDie(sizeof(geneScores), "E: OOM for result scores");
@@ -305,12 +344,20 @@ int main(void) {
         result->scores = mallocOrDie(causal->nbGenes * sizeof(SCORETYPE), "E: OOM for result scores");
 
         gbaCentrality(asym, causal, 0.5, result);
-        printf("Asym scores\n");
+        printf("ASYM scores\n");
         printScores(result);
+        // compare with expected:
+        if ((result->scores[0] == 0.5) && (result->scores[1] == 1) && (fabsf(result->scores[2] - 0.1f) < EPSILON) &&
+            (fabsf(result->scores[3] - 0.05f) < EPSILON) && (fabsf(result->scores[4] - 0.05f) < EPSILON) &&
+            (fabsf(result->scores[5] - 0.05f) < EPSILON) && (fabsf(result->scores[6] - 0.05f) < EPSILON))
+            printf("AOK, SCORES ARE AS EXPECTED\n");
+        else
+            printf("ERROR: SCORES FOR ASYM network ARE WRONG!\n");
         freeNetwork(asym);
         freeScores(causal);
         freeScores(result);
     }
+    printf("\n#############################################################################\n");
 
     return(0);
 }
